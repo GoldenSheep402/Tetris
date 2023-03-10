f95d31fcd7a2507f6b48c7f4c453fbc72da110de
GoldenSheep <805069152@qq.com>
2023-03-10 23:16:05 +0800

    feat: add ClearFullRows()

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"os/exec"
+	"runtime"
 	"tetris/define"
 	"time"
 )
@@ -38,24 +40,26 @@ func (g *Game) NewTetrominoIn() {
 
 func (g *Game) DropTetromino() {
 	// 俄罗斯方块下落
-	top := 0
-	button := 39
 	dropHeight := 999
 
 	// 下落高度
 	for x := 0; x < 15; x++ {
-		for y := 0; y < 4; y++ {
+		top := 0
+		button := 40
+		for y := 0; y < 3; y++ {
 			if g.board[y][x] == 1 {
-				top = y
+				if top < y {
+					top = y
+				}
 			}
 		}
-
-		for y := 39; y >= 36; y-- {
+		for y := 39; y >= 4; y-- {
 			if g.board[y][x] == 1 {
-				button = y
+				if button > y {
+					button = y
+				}
 			}
 		}
-
 		if dropHeight > (button - top) {
 			dropHeight = button - top
 		}
@@ -65,37 +69,75 @@ func (g *Game) DropTetromino() {
 	for x := 0; x < 15; x++ {
 		for y := 3; y >= 0; y-- {
 			if g.board[y][x] == 1 {
-				g.board[y+dropHeight][x] = 1
+				g.board[y+dropHeight-1][x] = 1
 				g.board[y][x] = 0
 			}
 		}
 	}
-	g.PrintBoard()
+}
+
+func (g *Game) ClearFullRows() {
+	// 消除满行
+	clearRows := 0
+	flag := true
+	
+	for y := 39; y >= 0; y-- {
+		for x := 0; x < 15; x++ {
+			if g.board[y][x] == 1 {
+				flag = false
+				break
+			}
+			if flag {
+				clearRows++
+				g.score += 100
+			}
+		}
+	}
+
+	for i := 0; i < clearRows; i++ {
+		for x := 0; x < 15; x++ {
+			for y := 39; y >= 0; y-- {
+				if g.board[y][x] == 1 && (y+i+1) < 40 {
+					g.board[y+i+1][x] = 1
+					g.board[y][x] = 0
+				}
+			}
+		}
+	}
 }
 
 func (g *Game) Start() {
 	for {
 		g.NewTetrominoIn()
-
 		g.PrintBoard()
+
 		// 等待用户按下回车键
 		reader := bufio.NewReader(os.Stdin)
-		char, _, err := reader.ReadRune()
+		_, _, err := reader.ReadRune()
+
 		if err != nil {
-			// 发生错误，退出循环
+
 			break
+		} else {
+			// 检查操作系统是否是 Windows
+			if runtime.GOOS == "windows" {
+				// 在 Windows 上清除控制台屏幕的内容
+				cmd := exec.Command("cmd", "/c", "cls")
+				cmd.Stdout = os.Stdout
+				cmd.Run()
+
+				// 执行 DropTetromino 函数
+				g.DropTetromino()
+				g.ClearFullRows()
+			}
 		}
-		if char == '\n' {
-			// 用户按下回车键，退出循环
-			g.DropTetromino()
-		}
-		//g.PrintBoard()
 	}
+	fmt.Println("Game Over!")
 }
 
 func (g *Game) PrintBoard() {
 	// 打印游戏板
-
+	fmt.Print("--------------------------------\n")
 	for i := range g.board {
 		fmt.Print("|")
 		for j := range g.board[i] {
@@ -127,7 +169,12 @@ func RandomTetromino() [][]int {
 }
 
 func main() {
-	game := Game{}
+	game := Game{
+		board:  make([][]int, 40),
+		active: make([][]int, 4),
+		score:  0,
+		level:  1,
+	}
 	game.BoardInit()
 	game.Start()
 }
